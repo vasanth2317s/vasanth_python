c16338ee60c83b6aa56fc090f08c173b6c86ee91
vasanth2317s <vasanth2317sss@gmail.com>
2024-02-04 16:04:29 +0530

    set

--- set.py
+++ set.py
@@ -48,3 +48,14 @@ b={"age":21,"course":"python"}
 a.update(b)
 print(a)
 print(a.items())
+print(a.pop("id"))
+print(a)
+
+print(a.popitem())
+print(a)
+
+print(a.clear())
+print(a)
+
+del a["name"]
+print(a)
